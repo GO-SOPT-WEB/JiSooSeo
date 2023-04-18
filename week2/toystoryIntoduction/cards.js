d1e3faa0295de3632dd2b7c5708408628029bd52
Seojisoo20191941 <amy0480@naver.com>
2023-04-18 20:56:05 +0900

    [ feat ] 카드 내용 상수로 빼기

---
+++ week2/toystoryIntoduction/cards.js
@@ -0,0 +1,60 @@
+const data = {
+  cards: [
+    {
+      id: 1,
+      name: '우디',
+      hashtags: ['카우보이', '우디_프라이드', '두손_번쩍_들어!'],
+      img: 'assets/image/woody.png',
+    },
+    {
+      id: 2,
+      name: '버즈',
+      hashtags: ['버즈_라이트이어', '우주비행사', '무한한공간_저_너머로'],
+      img: 'assets/image/buz.png',
+    },
+    {
+      id: 3,
+      name: '제시',
+      hashtags: ['카우걸', '버즈여친', '사이드킥'],
+      img: 'assets/image/jessie.png',
+    },
+    {
+      id: 4,
+      name: '보핍',
+      hashtags: ['양치기_아가씨', '양', '조명등'],
+      img: 'assets/image/bopeep.png',
+    },
+    {
+      id: 5,
+      name: '랏소',
+      hashtags: ['나쁜놈', '핑크_딸기곰', '케어베어'],
+      img: 'assets/image/lotso.png',
+    },
+    {
+      id: 6,
+      name: '햄',
+      hashtags: ['돼지', '저금통', '꿀꿀'],
+      img: 'assets/image/hamm.png',
+    },
+    {
+      id: 7,
+      name: '포테이토 부부',
+      hashtags: ['미스터_포테토', '미스_포테토', '감자'],
+      img: 'assets/image/potato.png',
+    },
+    {
+      id: 8,
+      name: '알린',
+      hashtags: ['외계인', '하~이', '트리오'],
+      img: 'assets/image/alien.png',
+    },
+    {
+      id: 9,
+      name: '포키',
+      hashtags: ['포크', '눈물', '핸드'],
+      img: 'assets/image/forky.png',
+    },
+  ],
+};
+
+export { data };
