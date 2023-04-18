d08233adc3994fe57323649aa68c44cbb4acc8ca
Seojisoo20191941 <amy0480@naver.com>
2023-04-19 02:29:28 +0900

    [ feat ] 체크박스 체크 시 배열에 담기 구현

--- week2/toystoryIntoduction/js/main.js
+++ week2/toystoryIntoduction/js/main.js
@@ -1,19 +1,11 @@
-import { data } from './cards.js';
+//네비게이션 태그 필터링
+function selectCategory() {
+  const check = document.getElementsByName('category');
 
-{
-  data.cards.map(
-    ({ id, name, hashtags, img }) =>
-      (document.querySelector('.card__section').innerHTML += `
-    <article class="card">
-    <header class="card__title">${name}</header>
-    <ul class="card__tags">
-        <li class="card__tag">${hashtags[0]}</li>
-        <li class="card__tag">${hashtags[1]}</li>
-        <li class="card__tag">${hashtags[2]}</li>
-    </ul>
-    <i class="card__tag--plus--tag">+</i>
-    <img src=${img} alt=${name} class="card__img" />
-    <i class="card__icon--like">♥︎</i>
-  </article>`)
-  );
+  let check_box = [];
+  for (var i = 0; i < check.length; i++) {
+    if (check[i].checked == true) {
+      check_box.push(check[i].value);
+    }
+  }
 }
---
+++ week2/toystoryIntoduction/js/map.js
@@ -0,0 +1,20 @@
+import { data } from './cards.js';
+
+// 카드 map 함수
+{
+  data.cards.map(
+    ({ id, name, hashtags, img }) =>
+      (document.querySelector('.card__section').innerHTML += `
+    <article class="card">
+    <header class="card__title">${name}</header>
+    <ul class="card__tags">
+        <li class="card__tag">${hashtags[0]}</li>
+        <li class="card__tag">${hashtags[1]}</li>
+        <li class="card__tag">${hashtags[2]}</li>
+    </ul>
+    <i class="card__tag--plus--tag">+</i>
+    <img src=${img} alt=${name} class="card__img" />
+    <i class="card__icon--like">♥︎</i>
+  </article>`)
+  );
+}
