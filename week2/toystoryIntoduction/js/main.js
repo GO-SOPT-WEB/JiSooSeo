b2eb86e748222b3e0607ffab60f4f4047cdede2f
Seojisoo20191941 <amy0480@naver.com>
2023-04-19 21:42:22 +0900

    [ etc ] 불필요 코드 삭제

--- week2/toystoryIntoduction/js/main.js
+++ week2/toystoryIntoduction/js/main.js
@@ -19,8 +19,6 @@ function checkCategory() {
 
 function selectCategory() {
   checkCategory();
-  //   selected_category = [...check_box];
-  //console.log(check_box);
   sortCategory(check_box);
 }
 
@@ -47,22 +45,7 @@ function deleteCategory(value) {
       check[i].checked = false;
     }
   }
-  //   sortCategory(check_box);
 }
-// var obj = {};
-
-// Object.defineProperty(obj, 'num', {
-//   get: function () {
-//     return this.value;
-//   },
-//   set: function (newValue) {
-//     // TODO 값 변경 시 처리할 내용
-//     this.value = newValue;
-//   },
-// });
-
-// obj.num = 13; // 객체obj의 변수num 값 13으로 세팅
-// obj.num; // 13 출력
 
 //해시태그 모달
 function showHashtagsModal(id) {
