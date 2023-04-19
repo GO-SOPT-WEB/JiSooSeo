21ee65c0033ef1b638647b8ff665c054c937c39a
Seojisoo20191941 <amy0480@naver.com>
2023-04-19 21:19:03 +0900

    [ fix ] 해시태그 모달 각 카드에 맞게 열고 닫히도록 id 넘겨주기

--- week2/toystoryIntoduction/js/main.js
+++ week2/toystoryIntoduction/js/main.js
@@ -1,5 +1,5 @@
 //네비게이션 태그 필터링
-//let selected_category = [];
+// let selected_category = [];
 
 function checkCategory() {
   const check = document.querySelectorAll("input[name='category']");
@@ -8,7 +8,6 @@ function checkCategory() {
   for (let i = 0; i < check.length; i++) {
     if (check[i].checked == true) {
       check_box.push(check[i].value);
-      //selected_category.push(check[i].value);
       let cheked_tag = document.querySelector(`#${check[i].value}`);
       cheked_tag.style.display = 'flex';
     } else {
@@ -16,14 +15,35 @@ function checkCategory() {
       cheked_tag.style.display = 'none';
     }
   }
-
-  return check_box;
 }
 
 function selectCategory() {
   checkCategory();
-  // console.log(check_box);
+  //   selected_category = [...check_box];
+  //console.log(check_box);
+  sortCategory(check_box);
 }
+
+function sortCategory(check_box) {
+  const cards = document.getElementsByClassName('card');
+
+  if (!check_box.includes('total')) {
+    // for (let i = 0; i < check_box.length; i++) {
+    for (let j = 0; j < cards.length; j++) {
+      let card_value = cards[j].getAttribute('value');
+      if (check_box.includes(card_value)) {
+        let cards_category = document.querySelector(`.${card_value}`);
+        cards_category.style.display = 'flex';
+      } else {
+        let cards_category = document.querySelector(`.${card_value}`);
+        cards_category.style.display = 'none';
+      }
+    }
+    // }
+  }
+}
+// selectCategory();
+// console.log(selected_category);
 
 //태그 x 버튼 클릭 시 삭제
 function deleteCategory(value) {
@@ -35,6 +55,7 @@ function deleteCategory(value) {
       check[i].checked = false;
     }
   }
+  //   sortCategory(check_box);
 }
 // var obj = {};
 
@@ -52,10 +73,10 @@ function deleteCategory(value) {
 // obj.num; // 13 출력
 
 //해시태그 모달
-function showHashtagsModal() {
-  document.querySelector('.modal').style.display = 'flex';
+function showHashtagsModal(id) {
+  document.querySelectorAll('.modal')[id].style.display = 'flex';
 }
 
-function closeHashtagModal() {
-  document.querySelector('.modal').style.display = 'none';
+function closeHashtagModal(id) {
+  document.querySelectorAll('.modal')[id].style.display = 'none';
 }
--- week2/toystoryIntoduction/js/map.js
+++ week2/toystoryIntoduction/js/map.js
@@ -4,16 +4,16 @@ import { data } from './cards.js';
 {
   data.cards.map(
     ({ id, name, hashtags, img, category }) =>
-      //  selectCategory().includes(category) &&
+      // check_box.includes(category) &&
       (document.querySelector('.card__section').innerHTML += `
-    <article class="card">
+    <article class="card ${category}" value=${category}>
       <article class="modal">
       <ul class="modal__hashtags">
         <li class="modal__hashtag">${hashtags[0]}</li>
         <li class="modal__hashtag">${hashtags[1]}</li>
         <li class="modal__hashtag">${hashtags[2]}</li>
       </ul>
-      <i class="modal--close--tag" onclick="closeHashtagModal()">x</i>
+      <i class="modal--close--tag" onclick="closeHashtagModal(${id})">x</i>
     </article>
     <header class="card__title">${name}</header>
     <ul class="card__tags">
@@ -21,7 +21,7 @@ import { data } from './cards.js';
         <li class="card__tag">${hashtags[1]}</li>
         <li class="card__tag">${hashtags[2]}</li>
     </ul>
-    <i class="card__tag--plus--tag" onclick="showHashtagsModal()">+</i>
+    <i class="card__tag--plus--tag" onclick="showHashtagsModal(${id})">+</i>
     <img src=${img} alt=${name} class="card__img" />
     <i class="card__icon--like">♥︎</i>
   </article>`)
