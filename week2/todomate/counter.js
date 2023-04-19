41d52e985f6e728dbc45c548d0bbdbaa07088486
Seojisoo20191941 <amy0480@naver.com>
2023-04-20 03:59:09 +0900

    [ style ] 모달 구조 변경

--- week2/todomate/counter.js
+++ week2/todomate/counter.js
@@ -11,17 +11,28 @@ function solvedTodo() {
 }
 
 function openModal(header) {
-  let background = document.querySelector('.mobile');
-  let modal = document.querySelector('.modal');
+  // let background = document.querySelector('.mobile');
+  let modal = document.querySelector('.modal__wrapper');
 
-  background.style.filter = 'blur(0.3rem)';
+  // background.style.filter = 'blur(0.3rem)';
   modal.style.display = 'flex';
+
+  // let todo = document.querySelector('.modal--write--todo').target.value;
+  // console.log(todo);
 }
 
 function closeModal() {
-  let background = document.querySelector('.mobile');
-  let modal = document.querySelector('.modal');
+  // let background = document.querySelector('.mobile');
+  let modal = document.querySelector('.modal__wrapper');
 
-  background.style.filter = 'blur(0rem)';
+  // background.style.filter = 'blur(0rem)';
   modal.style.display = 'none';
 }
+
+function writeTodo(e) {
+  todo = e.target.value;
+
+  console.log(todo);
+}
+
+// function addTodo
