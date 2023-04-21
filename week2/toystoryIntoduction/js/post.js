74ce14b8e20da93a41acc753974a0d8792922f54
Seojisoo20191941 <amy0480@naver.com>
2023-04-22 01:30:18 +0900

    [ feat ] 새 카드 추가 구현

--- week2/toystoryIntoduction/js/post.js
+++ week2/toystoryIntoduction/js/post.js
@@ -1,8 +1,8 @@
 function setThumbnail(event) {
-  var reader = new FileReader();
+  let reader = new FileReader();
 
   reader.onload = function (event) {
-    var img = document.createElement('img');
+    let img = document.createElement('img');
     img.setAttribute('src', event.target.result);
     img.setAttribute('class', 'form__info__img');
     document.querySelector('.form__info__img--container').appendChild(img);
@@ -11,4 +11,26 @@ function setThumbnail(event) {
   document.querySelector('.form__info__img--add-img').style.display = 'none';
 
   reader.readAsDataURL(event.target.files[0]);
+}
+
+function submit() {
+  let name = document.querySelector('.form__info__input').value;
+  let hashtags = document.querySelector('.form__info__input').value;
+  let categorys = document.querySelector('.toy-category');
+  let category = categorys.options[categorys.selectedIndex].value;
+  let image = document.querySelector('.form__info__img').src;
+
+  localStorage.setItem('name', name);
+  localStorage.setItem('hashtags', hashtags);
+  localStorage.setItem('category', category);
+  localStorage.setItem('image', image);
+
+  var a = document.createElement('a');
+  a.href = './index.html';
+  document.body.appendChild(a);
+  a.click();
+  setTimeout((_) => {
+    window.URL.revokeObjectURL(url);
+  }, 60000);
+  a.remove();
 }
