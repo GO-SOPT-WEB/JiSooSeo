59faeebb1574d895406aa1f17ff55a4ec1c1718a
Seojisoo20191941 <amy0480@naver.com>
2023-05-05 02:24:26 +0900

    [ feat ] 카드 데이터 구현

---
+++ week3/findingGame/src/core/cards.js
@@ -0,0 +1,40 @@
+export const data = {
+  cards: [
+    {
+      id: 1,
+      img: 'https://item.kakaocdn.net/do/dbcd799aac800f6a680f1d2b04cee5688f324a0b9c48f77dbce3a43bd11ce785',
+    },
+    {
+      id: 2,
+      img: 'https://item.kakaocdn.net/do/dbcd799aac800f6a680f1d2b04cee5689f17e489affba0627eb1eb39695f93dd',
+    },
+    {
+      id: 3,
+      img: 'https://item.kakaocdn.net/do/dbcd799aac800f6a680f1d2b04cee568339e41ce89b663315d96faecd7cfd11b',
+    },
+    {
+      id: 4,
+      img: 'https://item.kakaocdn.net/do/dbcd799aac800f6a680f1d2b04cee568ac8e738cb631e72fdb9a96b36413984e',
+    },
+    {
+      id: 5,
+      img: 'https://item.kakaocdn.net/do/dbcd799aac800f6a680f1d2b04cee568ba2da8249bd9ffef143efb890203e009',
+    },
+    {
+      id: 6,
+      img: 'https://item.kakaocdn.net/do/8b6a9c77549f6d25298779490a64019f9f5287469802eca457586a25a096fd31',
+    },
+    {
+      id: 7,
+      img: 'https://item.kakaocdn.net/do/8b6a9c77549f6d25298779490a64019f15b3f4e3c2033bfd702a321ec6eda72c',
+    },
+    {
+      id: 8,
+      img: 'https://item.kakaocdn.net/do/49af77ed647962e739c3f26189979525ce9463e040a07a9462a54df43e1d73f1',
+    },
+    {
+      id: 9,
+      img: 'https://item.kakaocdn.net/do/1d495862f49c38232ca8b6cc6a9679a0effd194bae87d73dd00522794070855d',
+    },
+  ],
+};
