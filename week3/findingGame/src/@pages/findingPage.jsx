907509f8e706d95bf60b51e79a67a42df093bfe1
Seojisoo20191941 <amy0480@naver.com>
2023-05-05 16:43:07 +0900

    [ fix ] 데이터 수정

--- week3/findingGame/src/@pages/findingPage.jsx
+++ week3/findingGame/src/@pages/findingPage.jsx
@@ -2,48 +2,48 @@ import CardList from '../@components/cardList';
 import LevelButton from '../@components/levelButton';
 import MainHeader from '../@components/mainHeader';
 import ResetButton from '../@components/resetButton';
-// import { CARDS_LIST } from '../core/cardsData';
+import { CARDS_LIST } from '../core/cardsData';
 import { useEffect, useState } from 'react';
+import { levelType } from '../core/levelType';
 
 export default function FindingPage() {
-  // const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState([]);
   // const [newCard, setNewCard] = useState({
   //   id: '',
   //   img: '',
   // });
   // const { id, img } = newCard;
 
-  // function mixCards(level) {
-  //   let randomNum = [];
-  //   let newCardData = JSON.parse(JSON.stringify(CARDS_LIST)); //카드데이터 깊은 복사
+  function mixCards(level) {
+    let randomNum = [];
+    let newCardData = JSON.parse(JSON.stringify(CARDS_LIST)); //카드데이터 깊은 복사
 
-  //   //1~9 수 중에서 level개만큼 랜덤 숫자 뽑기
-  //   for (let i = 0; i < level; i++) {
-  //     randomNum.push(Math.floor(Math.random() * (9 - 1) + 1));
-  //   }
+    //1~9 수 중에서 level개만큼 랜덤 숫자 뽑기
+    for (let i = 0; i < level; i++) {
+      randomNum.push(Math.floor(Math.random() * (8 - 0) + 0));
+    }
 
-  //   // console.log(newCardData);
-  //   //랜덤 숫자와 같은 id를 가진 데이터 가져오기
-  //   //randomNum.forEach(() => cardData);
-  //   // for (let i = 0; i < randomNum.length; i++) {
-  //   //   console.log(newCardData);
-  //   //   //  cardData.filter((cardData.id)=>(cardData.id));
+    // console.log(newCardData);
+    //랜덤 숫자와 같은 id를 가진 데이터 가져오기
+    //randomNum.forEach(() => cardData);
+    // for (let i = 0; i < randomNum.length; i++) {
+    //   console.log(newCardData);
+    //   //  cardData.filter((cardData.id)=>(cardData.id));
 
-  //   for (let i = 0; i < newCardData.length; i++) {
-  //     for (let j = 0; j < level; j++) {
-  //       if (newCardData[i].id === randomNum[j]) {
-  //         setCards([...cards, newCardData[i]]);
-  //       }
-  //     }
-  //   }
-  // }
+    for (let i = 0; i < level; i++) {
+      console.log('level' + level);
+      console.log('randomNum' + randomNum);
+      setCards((cards) => [...cards, newCardData[randomNum[i]]]);
+    }
+  }
 
-  // mixCards(5);
-  const [level, setLevel] = useState(0);
-  // eslint-disable-next-line no-unused-vars
+  console.log(cards);
+  const [level, setLevel] = useState(levelType.EASY);
   const [correct, setCorrect] = useState(0);
 
-  useEffect(() => {}, [level]);
+  useEffect(() => {
+    mixCards(level);
+  }, []);
 
   return (
     <>
