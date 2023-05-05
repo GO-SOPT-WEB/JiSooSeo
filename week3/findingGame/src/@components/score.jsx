654b5c3536d3530115dd17a773bcbe48566adf4a
Seojisoo20191941 <amy0480@naver.com>
2023-05-05 21:05:34 +0900

    [ feat ] 카드 한 쌍 맞출 때마다 스코어 반짝이도록 구현

--- week3/findingGame/src/@components/score.jsx
+++ week3/findingGame/src/@components/score.jsx
@@ -1,14 +1,19 @@
+import { useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 
 export default function Score(props) {
   const { level, correct } = props;
+  const [correctOneCard, setCorrectOneCard] = useState(false);
 
-  function checkAllCorrect() {
-    return correct === level;
-  }
+  useEffect(() => {
+    correct !== 0 && setCorrectOneCard(true);
+    setTimeout(() => {
+      setCorrectOneCard(false);
+    }, '2000');
+  }, [correct]);
 
   return (
-    <ScoreText checkAllCorrect={checkAllCorrect()}>
+    <ScoreText correctOneCard={correctOneCard}>
       {correct}/{level}
     </ScoreText>
   );
@@ -18,14 +23,16 @@ const BlinkEffect = keyframes`
   50% {
     opacity: 0;
   }
+  100%{
+    opacity: 1;
+  }
 `;
 
 const ScoreText = styled.p`
   margin-top: 1rem;
 
-  color: ${({ checkAllCorrect, theme }) =>
-    checkAllCorrect ? theme.colors.blue : theme.colors.grey};
+  color: ${({ correctOneCard, theme }) => (correctOneCard ? theme.colors.blue : theme.colors.grey)};
   ${({ theme }) => theme.fonts.title};
 
-  animation: ${BlinkEffect} ${({ checkAllCorrect }) => (checkAllCorrect ? 1 : 0)}s step-end infinite;
+  animation: ${BlinkEffect} ${({ correctOneCard }) => (correctOneCard ? 1 : 0)}s step-end infinite;
 `;
