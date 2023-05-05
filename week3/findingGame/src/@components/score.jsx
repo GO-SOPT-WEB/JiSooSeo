d3856aaf03cf9ee374fb8de8537001438e729814
Seojisoo20191941 <amy0480@naver.com>
2023-05-05 20:52:11 +0900

    [ feat ] 다 맞췄을 때 스코어 반짝이는 애니메이션 구현

--- week3/findingGame/src/@components/score.jsx
+++ week3/findingGame/src/@components/score.jsx
@@ -1,15 +1,31 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export default function Score(props) {
   const { level, correct } = props;
+
+  function checkAllCorrect() {
+    return correct === level;
+  }
+
   return (
-    <ScoreText>
+    <ScoreText checkAllCorrect={checkAllCorrect()}>
       {correct}/{level}
     </ScoreText>
   );
 }
 
+const BlinkEffect = keyframes`
+  50% {
+    opacity: 0;
+  }
+`;
+
 const ScoreText = styled.p`
-  color: ${({ theme }) => theme.colors.grey};
+  margin-top: 1rem;
+
+  color: ${({ checkAllCorrect, theme }) =>
+    checkAllCorrect ? theme.colors.blue : theme.colors.grey};
   ${({ theme }) => theme.fonts.title};
+
+  animation: ${BlinkEffect} ${({ checkAllCorrect }) => (checkAllCorrect ? 1 : 0)}s step-end infinite;
 `;
