dda3e7a4cec96a078e11f331c6197a41478abb63
Seojisoo20191941 <amy0480@naver.com>
2023-05-12 09:14:20 +0900

    [ feat ] 오늘의 날씨 데이터 불러오기 구현

--- week4/weather/src/@components/todayWeatherInfo.jsx
+++ week4/weather/src/@components/todayWeatherInfo.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function DetailWeatherInfo() {
+  const { area } = useParams();
+
+  useEffect(() => {
+    getTodayWeather(area);
+  }, []);
+
+  console.log(area);
+
   return <div>DetailWeatherInfo</div>;
 }
--- week4/weather/src/api/getWeather.jsx
+++ week4/weather/src/api/getWeather.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //오늘 날씨 조회
-export async function getTodayWeather() {
+export async function getTodayWeather(area) {
   try {
     const data = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${
@@ -16,7 +16,7 @@ export async function getTodayWeather() {
 }
 
 //주간 날씨 조회
-export async function getWeekWeather() {
+export async function getWeekWeather(area) {
   try {
     const data = await axios.get(
       `https://api.openweathermap.org/data/2.5/forecast?q=${area}&appid=${
