8c8655e70d176dc5172d02782fd3ce6eaf0beaee
Seojisoo20191941 <amy0480@naver.com>
2023-05-12 09:18:21 +0900

    [ feat ] api 검색창 구현

--- week4/weather/src/@pages/weatherPage.jsx
+++ week4/weather/src/@pages/weatherPage.jsx
@@ -6,6 +6,14 @@ import { Outlet } from "react-router-dom";
 export default function WeatherPage() {
   return (
     <PageLayout>
+      <>
+        <select name="weather">
+          <option value="today">오늘</option>
+          <option value="week">주간</option>
+        </select>
+        <input type="text" placeholder="영어로 도시명 ex) Seoul" />
+        <button type="button">날씨 검색</button>
+      </>
       <Outlet />
     </PageLayout>
   );
