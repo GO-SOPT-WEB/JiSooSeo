5e63ff1cda220ac8d07b67981535bd067de9f0d1
Seojisoo20191941 <amy0480@naver.com>
2023-05-12 11:20:30 +0900

    [ feat ] 날씨 카드 이미지 구현

--- week4/weather/src/@components/todayWeatherInfo.jsx
+++ week4/weather/src/@components/todayWeatherInfo.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getTodayWeather } from "../api/getWeather";
 import WeatherCard from "./weatherCard";
+import { WEATHER_TYPE } from "../core/weatherImg";
 
 export default function DetailWeatherInfo() {
   const { area } = useParams();
   const [weatherData, setWeatherData] = useState([]);
+  const [weatherImg, setWeatherImg] = useState("");
 
   async function fetchTodayWeatehrInfo() {
     const response = await getTodayWeather(area);
@@ -16,5 +18,15 @@ export default function DetailWeatherInfo() {
     fetchTodayWeatehrInfo();
   }, []);
 
-  return <WeatherCard weatherData={weatherData} />;
+  useEffect(() => {
+    console.log(area);
+    fetchTodayWeatehrInfo();
+  }, [area]);
+
+  useEffect(() => {
+    weatherData?.weather &&
+      setWeatherImg(WEATHER_TYPE.filter((w) => w.description === weatherData?.weather[0].description)[0].imgURL);
+  }, [weatherData]);
+
+  return <WeatherCard weatherData={weatherData} weatherImg={weatherImg} />;
 }
--- week4/weather/src/@components/weatherCard.jsx
+++ week4/weather/src/@components/weatherCard.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { WEATHER_TYPE } from "../core/weatherImg";
 
 export default function WeatherCard(props) {
-  const { weatherData } = props;
+  const { weatherData, weatherImg } = props;
   const { name, main, clouds } = weatherData;
 
   return (
-    <>
+    <WeatherCardWrapper>
       <h1>{name}</h1>
+      <img src={weatherImg} alt={name} />
       <p>온도 | {main?.temp}</p>
       <p>체감 온도 | {main?.feels_like}</p>
       <p>
         최저/최고 | {main?.temp_min}/{main?.temp_max}
       </p>
       <p>구름 | {clouds?.all}</p>
-    </>
+    </WeatherCardWrapper>
   );
 }
+
+const WeatherCardWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 2rem;
+
+  border: 0.3rem solid ${({ theme }) => theme.colors.blue};
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.colors.skyblue};
+  ${({ theme }) => theme.fonts.title}
+`;
