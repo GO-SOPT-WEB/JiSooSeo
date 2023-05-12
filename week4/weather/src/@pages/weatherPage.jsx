9b5121f8fc870a6a7b089a95ea5a9ad6e36bd3e4
Seojisoo20191941 <amy0480@naver.com>
2023-05-12 09:29:52 +0900

    [ design ] 도시 입력란 스타일

--- week4/weather/src/@pages/weatherPage.jsx
+++ week4/weather/src/@pages/weatherPage.jsx
@@ -12,8 +12,8 @@ export default function WeatherPage() {
           <option value="today">오늘</option>
           <option value="week">주간</option>
         </Select>
-        <input type="text" placeholder="영어로 도시명 ex) Seoul" />
-        <button type="button">날씨 검색</button>
+        <Input type="text" placeholder="영어로 도시명 ex) Seoul" />
+        <Button type="button">날씨 검색</Button>
       </DropBox>
       <Outlet />
     </PageLayout>
@@ -26,5 +26,30 @@ const DropBox = styled.section`
 `;
 
 const Select = styled.select`
+  margin-right: 1rem;
+  ${({ theme }) => theme.fonts.title}
+`;
+
+const Input = styled.input`
+  width: 25rem;
+  margin-right: 1rem;
+
+  border: 0.3rem solid ${({ theme }) => theme.colors.blue};
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.colors.skyblue};
+  ${({ theme }) => theme.fonts.title}
+`;
+
+const Button = styled.button`
+  width: 10rem;
+
+  outline: none;
+  border: none;
+  &:focus {
+    outline: none;
+    border: none;
+  }
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.colors.blue};
   ${({ theme }) => theme.fonts.title}
 `;
